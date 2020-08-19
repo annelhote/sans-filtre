cc5252877cdd7553808ede0897a1a4fa431fd978
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-19 19:31:15 +0200

    Deploy on github pages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Run
 
 `yarn serve`
+
+# Publish
+
+`yarn deploy`
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# See here : https://cli.vuejs.org/guide/deployment.html#github-pages
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy' --no-gpg-sign
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:annelhote/sans-filtre.git master:gh-pages
+
+cd -
