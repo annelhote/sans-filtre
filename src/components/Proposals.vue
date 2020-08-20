1a8c2ab683ef84a93b9945472678ac9202098f13
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-20 14:49:45 +0200

    Add color according to proposal's thematic

--- src/components/Proposals.vue
+++ src/components/Proposals.vue
@@ -1,29 +1,53 @@
 <template>
-  <div class="hello">
+  <div class="proposals">
     <div v-for="(chunk, index) in proposalChunks" :key="index" class="mb-2">
-      <div v-for="(proposal, index) in chunk" :key="index" :title="proposal.title" class="proposal mr-2 p-1 d-inline-block">
-        <font-awesome-icon :icon="retrieveIcon(proposal.color)" color="white" />
+      <div
+        v-for="(proposal, index) in chunk"
+        :key="index"
+        :title="proposal.title"
+        class="proposal mr-2 p-1 d-inline-block"
+        :style="{ backgroundColor: retrieveColor(proposal) }">
+        <font-awesome-icon :icon="retrieveIcon(proposal.color)" color="white"></font-awesome-icon>
       </div>
     </div>
     <div>
       <div>Légende :</div>
-      <div>
-        <font-awesome-icon icon="check" /> Appliquée intégralement
+      <div class="w-50 d-inline-block text-right pr-5">
+        <div>
+          <font-awesome-icon icon="check"></font-awesome-icon> Appliquée intégralement
+        </div>
+        <div>
+          <font-awesome-icon icon="check"></font-awesome-icon> Partiellement acceptée
+        </div>
+        <div>
+          <font-awesome-icon icon="comment"></font-awesome-icon> Débattue
+        </div>
+        <div>
+          <font-awesome-icon icon="exclamation"></font-awesome-icon> En danger
+        </div>
+        <div>
+          <font-awesome-icon icon="times"></font-awesome-icon> Rejetée
+        </div>
+        <div>
+          <font-awesome-icon icon="minus"></font-awesome-icon> Pas encore étudiée
+        </div>
       </div>
-      <div>
-        <font-awesome-icon icon="check" /> Partiellement acceptée
-      </div>
-      <div>
-        <font-awesome-icon icon="comment" /> Débattue
-      </div>
-      <div>
-        <font-awesome-icon icon="exclamation" /> En danger
-      </div>
-      <div>
-        <font-awesome-icon icon="times" /> Rejetée
-      </div>
-      <div>
-        <font-awesome-icon icon="minus" /> Pas encore étudiée
+      <div class="w-50 d-inline-block text-left pl-5">
+        <div>
+          <font-awesome-icon icon="circle" color="red"></font-awesome-icon> Se loger
+        </div>
+        <div>
+          <font-awesome-icon icon="circle" color="green"></font-awesome-icon> Se nourrir
+        </div>
+        <div>
+          <font-awesome-icon icon="circle" color="yellow"></font-awesome-icon> Produire et travailler
+        </div>
+        <div>
+          <font-awesome-icon icon="circle" color="blue"></font-awesome-icon> Se déplacer
+        </div>
+        <div>
+          <font-awesome-icon icon="circle" color="orange"></font-awesome-icon> Consommer
+        </div>
       </div>
     </div>
   </div>
@@ -32,7 +56,8 @@
 <script>
 import _ from 'lodash'
 
-import proposals from '../data/proposals.json';
+import proposals from '../data/proposals.json'
+import thematics from '../data/thematics.json'
 
 export default {
   name: 'HelloWorld',
@@ -72,6 +97,30 @@ export default {
           icon = 'check'
       }
       return icon
+    },
+    retrieveColor (proposal) {
+      let color = null
+      let thematic = null
+      const plop = _.find(thematics, { title: proposal.title })
+      if (plop) thematic = plop.thematic
+      switch (thematic) {
+        case 'Se loger':
+          color = 'red'
+          break
+        case 'Se nourrir':
+          color = 'green'
+          break
+        case 'Produire et travailler':
+          color = 'yellow'
+          break
+        case 'Se déplacer':
+          color = 'blue'
+          break
+        case 'Consommer':
+          color = 'orange'
+          break
+      }
+      return color
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck, faComment, faExclamation, faMinus, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faCircle, faComment, faExclamation, faMinus, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 library.add(faCheck)
+library.add(faCircle)
 library.add(faComment)
 library.add(faExclamation)
 library.add(faMinus)
