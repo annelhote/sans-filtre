6f15ec7cda9b565d808ee7e354d168555edf4580
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-20 14:13:22 +0200

    Add scraper about the thematics of the proposals

---
+++ scrapping/thematics.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs')
+
+const url = 'https://www.franceinter.fr/environnement/les-150-propositions-de-la-convention-citoyenne-sur-le-climat';
+const file = 'src/data/thematics.json'
+
+axios(url)
+  .then(async response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const children = $('.content-body').children()
+    let thematic = ''
+    let goal = ''
+    const thematics = []
+
+    children.each(function (_, child) {
+      switch (child.name) {
+        case 'h2':
+          thematic = $(child).text()
+          break
+        case 'p':
+          goal = $(child).text()
+          break
+        case 'ul':
+          $(child).find('li').each(function (_, li) {
+            const measure = $(li).text()
+            thematics.push({ thematic, goal, measure })
+          })
+      }
+    })
+
+    const data = JSON.stringify(thematics, null, 2);
+    fs.writeFileSync(file, data);
+  })
+  .catch(console.error);
