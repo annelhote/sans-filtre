19f542b7446a09849cde856868b0954663e05828
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-20 14:19:27 +0200

    Rename HelloWorld component into Proposals

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <proposals></proposals>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Proposals from './components/Proposals'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Proposals
   }
 }
 </script>
